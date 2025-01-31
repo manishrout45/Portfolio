c009714ca1d2bac644638a9349c7ed429e741eb3
manishrout45 <manishkumarrout45@gmail.com>
2025-01-31 13:37:47 +0530

    updated

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+//navmenu
+const mobileMenuButton = document.getElementById('mobile-menu-button');
+    const mobileNav = document.getElementById('mobile-nav');
+    const closeMenu = document.getElementById('close-menu');
+
+    mobileMenuButton.addEventListener('click', () => {
+        mobileNav.classList.remove('-translate-y-full');
+    });
+
+    closeMenu.addEventListener('click', () => {
+        mobileNav.classList.add('-translate-y-full');
+    });
+
+
+
+
 // Add scroll animations
 document.addEventListener('scroll', () => {
     const sections = document.querySelectorAll('section');
@@ -8,3 +24,6 @@ document.addEventListener('scroll', () => {
         }
     });
 });
+
+
+
